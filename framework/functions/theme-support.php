1f755cef1bfede3bd8c30077914b5bcff818b8af
Steve Ariss <steve@arissberg.com>
2020-01-19 14:06:00 -0500

    Adding some additional elements

--- framework/functions/theme-support.php
+++ framework/functions/theme-support.php
@@ -6,104 +6,113 @@
  *
  */
 
-if ( ! function_exists( 'ab_theme_support' ) ) :
-
-  function ab_theme_support() {
-    add_theme_support( 'automatic-feed-links' );
-    add_theme_support( 'post-thumbnails' );
-    add_theme_support( 'title-tag' ); // Enable plugins to manage the document title
-    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
-    add_theme_support( 'post-formats', array( 'gallery', 'video', 'image' ) );
+if (!function_exists('ab_theme_support')):
+  function ab_theme_support()
+  {
+    add_theme_support('automatic-feed-links');
+    add_theme_support('post-thumbnails');
+    add_theme_support('title-tag'); // Enable plugins to manage the document title
+    add_theme_support('html5', array(
+      'comment-list',
+      'comment-form',
+      'search-form',
+      'gallery',
+      'caption'
+    ));
+    add_theme_support('post-formats', array('gallery', 'video', 'image'));
 
     // Add RSS links to head
-    add_theme_support( 'automatic-feed-links' );
+    add_theme_support('automatic-feed-links');
 
     //Add Post Type Support
-    add_post_type_support('page','excerpt');
+    add_post_type_support('page', 'excerpt');
 
     // Add theme support for selective refresh for widgets.
-    add_theme_support( 'customize-selective-refresh-widgets' );
+    add_theme_support('customize-selective-refresh-widgets');
 
     // Add support for Block Styles.
-    add_theme_support( 'wp-block-styles' );
+    add_theme_support('wp-block-styles');
 
     // Add support for full and wide align in Gutenberg.
-    add_theme_support( 'align-wide' );
+    add_theme_support('align-wide');
 
     // Add support for editor styles.
-    add_theme_support( 'editor-styles' );
+    add_theme_support('editor-styles');
 
     // Enqueue editor styles.
-    //add_editor_style( 'public/css/style-editor.css' );
-    //add_editor_style( 'public/css/site.css' );
+    add_editor_style('public/css/style-editor.css');
+    add_editor_style('public/css/site.css');
 
     //Customizer
-    add_theme_support( 'custom-logo' );
+    add_theme_support('custom-logo');
 
     // Add custom editor font sizes.
-    add_theme_support(
-      'editor-font-sizes',
+    add_theme_support('editor-font-sizes', array(
       array(
-        array(
-          'name'      => __( 'Small', 'arissberg' ),
-          'shortName' => __( 'S', 'arissberg' ),
-          'size'      => 19.5,
-          'slug'      => 'small',
-        ),
-        array(
-          'name'      => __( 'Normal', 'arissberg' ),
-          'shortName' => __( 'M', 'arissberg' ),
-          'size'      => 22,
-          'slug'      => 'normal',
-        ),
-        array(
-          'name'      => __( 'Large', 'arissberg' ),
-          'shortName' => __( 'L', 'arissberg' ),
-          'size'      => 36.5,
-          'slug'      => 'large',
-        ),
-        array(
-          'name'      => __( 'Huge', 'arissberg' ),
-          'shortName' => __( 'XL', 'arissberg' ),
-          'size'      => 49.5,
-          'slug'      => 'huge',
-        ),
+        'name' => __('Small', 'arissberg'),
+        'shortName' => __('S', 'arissberg'),
+        'size' => 19.5,
+        'slug' => 'small'
+      ),
+      array(
+        'name' => __('Normal', 'arissberg'),
+        'shortName' => __('M', 'arissberg'),
+        'size' => 22,
+        'slug' => 'normal'
+      ),
+      array(
+        'name' => __('Large', 'arissberg'),
+        'shortName' => __('L', 'arissberg'),
+        'size' => 36.5,
+        'slug' => 'large'
+      ),
+      array(
+        'name' => __('Huge', 'arissberg'),
+        'shortName' => __('XL', 'arissberg'),
+        'size' => 49.5,
+        'slug' => 'huge'
       )
-    );
+    ));
 
     // Editor color palette.
-    add_theme_support(
-      'editor-color-palette',
+    add_theme_support('editor-color-palette', array(
       array(
-        array(
-          'name'  => __( 'Primary', 'arissberg' ),
-          'slug'  => 'primary',
-          'color' => hsl_to_hex( 'default' === get_theme_mod( 'primary_color' ) ? 199 : get_theme_mod( 'primary_color_hue', 199 ), 100, 33 ),
-        ),
-        array(
-          'name'  => __( 'Secondary', 'arissberg' ),
-          'slug'  => 'secondary',
-          'color' => hsl_to_hex( 'default' === get_theme_mod( 'primary_color' ) ? 199 : get_theme_mod( 'primary_color_hue', 199 ), 100, 23 ),
-        ),
-        array(
-          'name'  => __( 'Dark Gray', 'arissberg' ),
-          'slug'  => 'dark-gray',
-          'color' => '#111',
-        ),
-        array(
-          'name'  => __( 'Light Gray', 'arissberg' ),
-          'slug'  => 'light-gray',
-          'color' => '#767676',
-        ),
-        array(
-          'name'  => __( 'White', 'arissberg' ),
-          'slug'  => 'white',
-          'color' => '#FFF',
-        ),
+        'name' => __('Primary', 'arissberg'),
+        'slug' => 'primary',
+        'color' => hsl_to_hex(
+          'default' === get_theme_mod('primary_color')
+            ? 199
+            : get_theme_mod('primary_color_hue', 199),
+          100,
+          33
+        )
+      ),
+      array(
+        'name' => __('Secondary', 'arissberg'),
+        'slug' => 'secondary',
+        'color' => hsl_to_hex(
+          'default' === get_theme_mod('primary_color')
+            ? 199
+            : get_theme_mod('primary_color_hue', 199),
+          100,
+          23
+        )
+      ),
+      array(
+        'name' => __('Dark Gray', 'arissberg'),
+        'slug' => 'dark-gray',
+        'color' => '#111'
+      ),
+      array(
+        'name' => __('Light Gray', 'arissberg'),
+        'slug' => 'light-gray',
+        'color' => '#767676'
+      ),
+      array(
+        'name' => __('White', 'arissberg'),
+        'slug' => 'white',
+        'color' => '#FFF'
       )
-    );
-
+    ));
   }
-
 endif;
-
--- framework/scripts.php
+++ framework/scripts.php
@@ -3,40 +3,58 @@
     Queue Assets
 \* -------------------------------------------------------------------------- */
 
-function hop_queue_theme_assets() {
-  wp_enqueue_script( 'script-bundle', mix('/public/js/site.js'), array(), wp_get_theme()->get('Version'), true );
-  wp_enqueue_style( 'style-bundle', mix('/public/css/site.css'), array(), wp_get_theme()->get('Version') );
-};
-add_action( 'wp_enqueue_scripts', 'hop_queue_theme_assets' );
-
-
-function hop_gutenberg_scripts() {
-	wp_enqueue_script( 'be-editor', mix('/public/js/editor.js'), array( 'wp-blocks', 'wp-dom' ), wp_get_theme()->get('Version'), true);
+function ab_queue_theme_assets()
+{
+  wp_enqueue_script(
+    'script-bundle',
+    mix('/public/js/site.js'),
+    array(),
+    wp_get_theme()->get('Version'),
+    true
+  );
+  wp_enqueue_style(
+    'style-bundle',
+    mix('/public/css/site.css'),
+    array(),
+    wp_get_theme()->get('Version')
+  );
 }
-//add_action( 'enqueue_block_editor_assets', 'hop_gutenberg_scripts' );
+add_action('wp_enqueue_scripts', 'ab_queue_theme_assets');
 
-function remove_block_css(){
-  wp_dequeue_style( 'wp-block-library-theme' );
+function ab_gutenberg_scripts()
+{
+  wp_enqueue_script(
+    'be-editor',
+    mix('/public/js/editor.js'),
+    array('wp-blocks', 'wp-dom'),
+    wp_get_theme()->get('Version'),
+    true
+  );
 }
-add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 );
+//add_action( 'enqueue_block_editor_assets', 'ab_gutenberg_scripts' );
+
+function ab_remove_block_css()
+{
+  wp_dequeue_style('wp-block-library-theme');
+  wp_dequeue_style('wp-block-library');
+}
+//add_action('wp_enqueue_scripts', 'ab_remove_block_css', 100);
 
 /* -------------------------------------------------------------------------- *\
     defer or asynchronously load scripts
 \* -------------------------------------------------------------------------- */
-function js_async_attr($tag) {
-
+function js_async_attr($tag)
+{
   # add defer or async attribute to these scripts
-  $scripts_to_include = array(
-    'use.typekit.net',
-    'maps.googleapis.com'
-  );
+  $scripts_to_include = array('use.typekit.net', 'maps.googleapis.com');
 
-  foreach ( $scripts_to_include as $include_script ) {
-    if ( false == strpos($tag, $include_script ) )
+  foreach ($scripts_to_include as $include_script) {
+    if (false == strpos($tag, $include_script)) {
       return $tag;
+    }
   }
 
   # Defer or async all remaining scripts not excluded above
-  return str_replace( ' src', ' defer async src', $tag );
+  return str_replace(' src', ' defer async src', $tag);
 }
-add_filter( 'script_loader_tag', 'js_async_attr', 10 );
+add_filter('script_loader_tag', 'js_async_attr', 10);
