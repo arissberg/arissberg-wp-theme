4240055faca31945a9b50d8cc041245895d3e495
Steve Ariss <steve@arissberg.com>
2020-01-22 13:12:53 -0500

    minor updates

--- framework/partials/favicon.php
+++ framework/partials/favicon.php
@@ -5,16 +5,21 @@
  *
  * @return void
  */
-if ( ! function_exists('ab_add_favicon')) :
-  function ab_add_favicon() {
+if (!function_exists('ab_add_favicon')):
+  function ab_add_favicon()
+  {
     ?>
 
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= __t() . 'public/img/favicon/apple-touch-icon.png'; ?>">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= __t() . 'public/img/favicon/favicon-32x32.png'; ?>">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= __t() . 'public/img/favicon/favicon-16x16.png'; ?>">
-    <link rel="manifest" href="<?= __t() . 'public/img/favicon/site.webmanifest'; ?>">
-    <link rel="mask-icon" href="<?= __t() . 'public/img/favicon/safari-pinned-tab.svg'; ?>" color="#000000">
-    <link rel="shortcut icon" href="<?= __t() . 'public/img/favicon/favicon.ico'; ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= __t() .
+      'public/img/favicon/apple-touch-icon.png' ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= __t() .
+      'public/img/favicon/favicon-32x32.png' ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= __t() .
+      'public/img/favicon/favicon-16x16.png' ?>">
+    <link rel="manifest" href="<?= __t() .
+      'public/img/favicon/site.webmanifest' ?>">
+    <link rel="shortcut icon" href="<?= __t() .
+      'public/img/favicon/favicon.ico' ?>">
     <meta name="msapplication-TileColor" content="#000000">
     <meta name="theme-color" content="#000000">
 
@@ -22,6 +27,6 @@ if ( ! function_exists('ab_add_favicon')) :
   }
 endif;
 
-add_action( 'wp_head', 'ab_add_favicon' );
+add_action('wp_head', 'ab_add_favicon');
 
 ?>
